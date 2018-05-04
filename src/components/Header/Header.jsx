dfc98f27aef460e53157db8e19775f73b8a810e3
egorhormash <egorgormash@icloud.com>
2018-05-04 18:22:20 +0300

    Changed routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
-
-import './Header.scss';
+import { Route, NavLink } from 'react-router-dom';
+import './Header.css';
 
 class Header extends React.Component {
-  render() {
+    render() {
 
-    return (
-      <header className="app-header navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">Eda</a>
-        <div>
-          <button className="header__btn-cart"></button>
-          <a className="btn btn-secondary my-2 my-sm-0">Sign in</a>
-          <a className="btn btn-secondary my-2 my-sm-0">Sign up</a>
-        </div>
-      </header>
-    );
-  }
+        return (
+            <header className="header">
+                <a className="header__logo" href="#">Eda</a>
+                <div className="header-right">
+                    <button className="header__btn-cart"></button>
+                    <NavLink to='/signin' className="btn btn-secondary my-2 my-sm-0">Sign in</NavLink>
+                    <NavLink to='/signup' className="btn btn-secondary my-2 my-sm-0">Sign up</NavLink>
+                </div>
+            </header>
+        );
+    }
 }
 
 export default Header;
--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,19 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import App from './components/App/App.jsx';
 import Header from './components/Header/Header.jsx';
+import Main from './components/Main/Main.jsx';
 import Footer from './components/Footer/Footer.jsx';
 
 const Routes = () => (
-  <Router>
-    <App>
-      <Header />
-
-      <Footer />
-    </App>
-  </Router>
+    <Router>
+        <App>
+            <Header />
+            <Main />
+            <Footer />
+        </App>
+    </Router>
 );
 
 export default Routes;
