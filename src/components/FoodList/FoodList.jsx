155213faa8c13a64878c8ad06550f6b3d09ef649
egorhormash <egorgormash@icloud.com>
2018-05-04 18:20:28 +0300

    Add food component

---
+++ src/components/FoodList/FoodList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './FoodList.css';
+
+class FoodList extends React.Component {
+    render() {
+
+        return (
+            <div className="blocks">
+
+            </div>
+        );
+    }
+}
+
+export default FoodList;
