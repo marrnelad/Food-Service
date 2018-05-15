87e9769367be3f5ded45292fd7b61cd0d26376c2
egorhormash <egorgormash@icloud.com>
2018-05-15 09:59:23 +0300

    Add supplier controller

---
+++ server/controllers/supplierController.js
@@ -0,0 +1,37 @@
+import db from '../models/index.js';
+
+export function getSuppliers(req, res) {
+
+    return db.Supplier.findAll()
+        .then(function(suppliers) {
+            if (suppliers.length) {
+                return res.send(suppliers);
+            }
+            return res.send({
+                message: "Could not find any supplier."
+            });
+        })
+        .catch(function(error) {
+            console.log(error);
+            return res.send({
+                message: "Error retrieving suppliers."
+            });
+        });
+}
+
+export function createSupplier(req, res) {
+
+    return db.Supplier
+        .build({
+            name: req.body.name,
+            address: req.body.address,
+            phone: req.body.phone
+        })
+        .save()
+        .then(supplier => {
+            res.send(supplier);
+        })
+        .catch(error => {
+            message: "Could not create supplier."
+        });
+}
