4f3a3d009b16953d79d377a8d2941b8e6393a28c
egorhormash <egorgormash@icloud.com>
2018-05-15 11:07:03 +0300

    Fix some bugs

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
 import App from './components/App/App.jsx';
 import Header from './components/Header/Header.jsx';
 import Main from './components/Main/Main.jsx';
 import Footer from './components/Footer/Footer.jsx';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
+const createStoreWithMiddleware =  composeWithDevTools(applyMiddleware(thunk))(createStore);
+const store = createStoreWithMiddleware(reducers);
 
 const Routes = () => (
-    <Router>
-        <App>
-            <Header />
-            <Main />
-            <Footer />
-        </App>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <App>
+                <Header />
+                <Main />
+                <Footer />
+            </App>
+        </Router>
+    </Provider>
 );
 
 export default Routes;
