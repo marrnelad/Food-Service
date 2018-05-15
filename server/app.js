999c632d4fc6f296f6cf82466151018462afa069
egorhormash <egorgormash@icloud.com>
2018-05-15 10:00:06 +0300

    Change routes

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import Sequelize from 'sequelize';
-
+import apiRoutes from './routes/apiRoutes.js';
 import routes from './routes/index.js';
 
 const sequelize = new Sequelize('postgres://postgres:Egor-1997@localhost:5432/eda');
@@ -12,8 +12,7 @@ const app = express();
 app.use( bodyParser.urlencoded({ extended: true }) );
 app.use( bodyParser.json() );
 app.use( cors({ origin: '*' }) );
-
-app.use('/api', routes);
+app.use('/api', apiRoutes);
 
 app.listen(3001, () => {
     console.log('Server is running on port 3000');
