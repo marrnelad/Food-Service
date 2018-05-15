e25965a3132501ce705866e586b3ad038d60f3c0
egorhormash <egorgormash@icloud.com>
2018-05-15 10:02:29 +0300

    Add api routes

---
+++ server/routes/apiRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+import * as foodController from '../controllers/foodController.js';
+import * as suppliersController from '../controllers/suppliersController.js';
+
+const apiRouter = new express.Router();
+
+apiRouter.get('/food', foodController.getFood);
+
+apiRouter.post('/food', foodController.createFood);
+
+apiRouter.delete('/food/:idFood', foodController.deleteFood);
+
+apiRouter.get('/suppliers', suppliersController.getSuppliers);
+
+apiRouter.post('/suppliers', suppliersController.createSupplier);
+
+apiRouter.get('/suppliers/:idSupplier', foodController.getSuppliersFood);
+
+module.exports = apiRouter;
